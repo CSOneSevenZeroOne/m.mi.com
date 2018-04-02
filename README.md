341690cf23385ce8051b5916635fe65cf814cfbc
junxing0827 <junxing0827@qq.com>
2018-04-02 11:36:09 +0800

    购物车搭建与未登录组件

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # 基于Vue重写的小米商城
+
 ## 萌新代码
 
 ----
+
 ### 组件
 
 |组件名|路径|
 |-|-|
 |公用头部组件|entry/compoents/module/header.vue|
+|公用底部组件|entry/compoents/module/footer.vue|
+
+### 购物车
+
+* 入口文件：cart.entry.js
+* 出口文件：cart.bundle.js
+* 未登录组件：cart_unlogin.vue
---
+++ entry/cart.entry.js
@@ -0,0 +1,35 @@
+// 购物车入口文件
+
+// 导入模块
+import Vue from "vue";
+require("./css/base.css");
+import xheader from "./components/module/header.vue";
+import xfooter from "./components/module/footer.vue";
+
+import xcart_unlogin from "./components/cart_unlogin.vue";//未登录状态
+
+
+
+// 创建实例
+new Vue({
+    el: "#cart",
+    template:`
+        <div style="height:100%; width:100%" id="cart_page">
+            <xheader />
+            <xcart_unlogin />
+            <xfooter />
+        </div>
+    `,
+    components:{
+        xheader,
+        xfooter,
+        xcart_unlogin
+    },
+    methods:{
+        
+    },
+    mounted(){
+        
+    }
+
+});
