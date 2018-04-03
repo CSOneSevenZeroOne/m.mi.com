ed42a9c2d5148ee7462367fceca66b118b234107
EdwardGuynewt <edwardguynewt@sina.com>
2018-04-03 20:05:21 +0800

    page3页面完善

--- entry/components/index-all.vue
+++ entry/components/index-all.vue
@@ -10,29 +10,35 @@
     <div v-show="this.$store.state.index_page==2">
       <page2 />
     </div>
+    <div v-show="this.$store.state.index_page==3">
+      <page3 />
+    </div>
   </div>
 </template>
 
 <script>
-  import "../inconfont/iconfont.css"; //导入iconfont样式
+import "../inconfont/iconfont.css"; //导入iconfont样式
 
-  import hhl_header from "./index-header.vue"; //导入主页头部组件
-  import page0 from "./index-page0.vue"; //导入主页导航组件
-  import page1 from "./index-page1.vue";
-  import page2 from "./index-page2.vue";
+import hhl_header from "./index-header.vue"; //导入主页头部组件
+import page0 from "./index-page0.vue"; //导入主页导航组件
+import page1 from "./index-page1.vue";
+import page2 from "./index-page2.vue";
+import page3 from "./index-page3.vue";
 
-  export default {
-    data() {
-      return {};
-    },
-    components: {
-      hhl_header,
-      page0,
-      page1,
-      page2
-    }
-  };
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    hhl_header,
+    page0,
+    page1,
+    page2,
+    page3
+  }
+};
 </script>
 
 <style>
+
 </style>
---
+++ entry/components/index-page3.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="page3-box">
+        <autoplay />
+        <divide />
+        <img src="../images/indeximages/page3.1.webp" style="width:3.75rem;height:0.42rem">
+        <bigshop />
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <divide />
+        <img src="../images/indeximages/page3.2.png" style="width:3.75rem;height:0.42rem">
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <divide />
+        <img src="../images/indeximages/page3.3.png" style="width:3.75rem;height:0.42rem">
+        <bigshop />
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <divide />
+        <img src="../images/indeximages/page3.4.webp" style="width:3.75rem;height:0.42rem">
+        <bigshop />
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <divide />
+        <img src="../images/indeximages/page3.5.webp" style="width:3.75rem;height:0.42rem">
+        <bigshop />
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <divide />
+        <img src="../images/indeximages/page3.6.webp" style="width:3.75rem;height:0.42rem">
+        <bigshop />
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <divide />
+        <img src="../images/indeximages/page3.7.webp" style="width:3.75rem;height:0.42rem">
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <divide />
+        <img src="../images/indeximages/page3.8.webp" style="width:3.75rem;height:0.42rem">
+        <bigshop />
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <divide />
+        <img src="../images/indeximages/page3.9.webp" style="width:3.75rem;height:0.42rem">
+        <bigshop />
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <bigshop />
+        <divide />
+        <img src="../images/indeximages/page3.10.webp" style="width:3.75rem;height:0.42rem">
+        <bigshop />
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+            <shops></shops>
+        </div>
+        <divide />
+        <img src="../images/indeximages/page3.11.webp" style="width:3.75rem;height:0.42rem">
+        <div class="shops-list">
+            <shops></shops>
+            <shops></shops>
+        </div>
+    </div>
+</template>
+
+<script>
+    import bigshop from "./module/bigshop.vue";
+    import divide from "./module/divide_line.vue";
+    import autoplay from "./index-autoplay.vue";
+    import shops from "../components/module/shopInfo.vue";
+    export default {
+        components: {
+            bigshop,
+            autoplay,
+            divide,
+            shops
+        }
+    };
+</script>
+
+<style scoped>
+    .page3-box {
+        margin-top: 0.86rem;
+    }
+
+    .shops-list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+</style>
