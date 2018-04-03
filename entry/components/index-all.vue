76916f083ad1da24873d941dcea7457fb0df9fa5
EdwardGuynewt <edwardguynewt@sina.com>
2018-04-03 19:30:00 +0800

    page2页面完善

--- entry/components/index-all.vue
+++ entry/components/index-all.vue
@@ -2,33 +2,37 @@
   <div>
     <hhl_header />
     <div v-show="this.$store.state.index_page==0">
-        <page0 />
+      <page0 />
     </div>
     <div v-show="this.$store.state.index_page==1">
-        <page1 />
+      <page1 />
+    </div>
+    <div v-show="this.$store.state.index_page==2">
+      <page2 />
     </div>
   </div>
 </template>
 
 <script>
-import "../inconfont/iconfont.css"; //导入iconfont样式
+  import "../inconfont/iconfont.css"; //导入iconfont样式
 
-import hhl_header from "./index-header.vue"; //导入主页头部组件
-import page0 from "./index-page0.vue"; //导入主页导航组件
-import page1 from "./index-page1.vue";
+  import hhl_header from "./index-header.vue"; //导入主页头部组件
+  import page0 from "./index-page0.vue"; //导入主页导航组件
+  import page1 from "./index-page1.vue";
+  import page2 from "./index-page2.vue";
 
-export default {
-  data() {
-    return {};
-  },
-  components: {
-    hhl_header,
-    page0,
-    page1
-  }
-};
+  export default {
+    data() {
+      return {};
+    },
+    components: {
+      hhl_header,
+      page0,
+      page1,
+      page2
+    }
+  };
 </script>
 
 <style>
-
 </style>
--- entry/components/index-page2.vue
+++ entry/components/index-page2.vue
@@ -1,13 +1,109 @@
 <template>
-  
+  <div class="page2-box">
+      <autoplay />
+      <divide />
+      <img src="../images/indeximages/remaibaoping.webp" style="width:3.75rem;height:0.42rem">
+      <bigshop />
+      <div class="shops-list">
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+      </div>
+      <divide />
+      <img src="../images/indeximages/xinpingtuijian.webp" style="width:3.75rem;height:0.42rem">
+      <bigshop />
+      <div class="shops-list">
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+      </div>
+      <divide />
+      <img src="../images/indeximages/zhinengjiaju.webp" style="width:3.75rem;height:0.42rem">
+      <div class="shops-list">
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+      </div>
+      <divide />
+      <img src="../images/indeximages/jiankangjiance.png" style="width:3.75rem;height:0.42rem">
+      <div class="shops-list">
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+      </div>
+      <divide />
+      <img src="../images/indeximages/kuwanyizhi.webp" style="width:3.75rem;height:0.42rem">
+      <divide />
+      <div class="shops-list">
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+      </div>
+      <divide />
+      <img src="../images/indeximages/zhinenghuwai.webp" style="width:3.75rem;height:0.42rem">
+      <div class="shops-list">
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+      </div>
+      <divide />
+      <img src="../images/indeximages/shexiangdengju.png" style="width:3.75rem;height:0.42rem">
+      <div class="shops-list">
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+      </div>
+      <divide />
+      <img src="../images/indeximages/chaoliuyingyin.webp" style="width:3.75rem;height:0.42rem">
+      <div class="shops-list">
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+      </div>
+      <divide />
+      <img src="../images/indeximages/zhinengchuandai.png" style="width:3.75rem;height:0.42rem">
+      <div class="shops-list">
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+          <shops></shops>
+      </div>
+      <divide />
+  </div>
 </template>
-
 <script>
+import bigshop from "./module/bigshop.vue";
+import divide from "./module/divide_line.vue";
+import autoplay from "./index-autoplay.vue";
+import shops from "../components/module/shopInfo.vue";
 export default {
-  
+  components:{
+      bigshop,
+      autoplay,
+      divide,
+      shops
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.page2-box{
+    margin-top: .86rem;
+}
+.shops-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 </style>
+
