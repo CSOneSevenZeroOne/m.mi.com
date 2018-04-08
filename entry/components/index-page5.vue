41f04017af1542d81cca50ca66767ddc5c15dd0a
EdwardGuynewt <edwardguynewt@sina.com>
2018-04-08 09:13:51 +0800

    UpdateCode

--- entry/components/index-page5.vue
+++ entry/components/index-page5.vue
@@ -3,6 +3,17 @@
         <div style="background-color:rgb(192, 4, 54)">
             <img src="../images/indeximages/page5.1.jpg" style="width:100%;height:1.88rem;display:block" />
             <img src="../images/indeximages/page5.2.jpg" style="width:100%;height:1.03rem;display:block" />
+            <div class="imglist">
+                <div>
+                    <img src="../images/indeximages/page5.3.jpg" style="width:3.63rem;height:2.27rem;display:block">
+                    <div class="shopInfo">
+                        <div class="name">小米MIX 2S</div>
+                        <div class="price">￥3299起</div>
+                        <div class="intro">骁龙845 旗舰处理器，AI双摄相机</div>
+                        <div class="btn">立即预约</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -14,5 +25,13 @@
 <style scoped>
     .page5-box {
         margin-top: 0.86rem;
+    }
+    .imglist{
+        width:3.63rem;
+        margin-left: 0.055rem;
+    }
+    .shopInfo{
+        height: .88rem;
+        background-color: #ffffff;
     }
 </style>
